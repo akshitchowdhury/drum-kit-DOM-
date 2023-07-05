9691f20b5576104fa422e9cf2f5f55bb6f2ffe3e
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-05 12:48:51 +0530

    Add files via upload

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -24,8 +24,6 @@
 
 function onclick(){
     
-let audio = new Audio("./sounds/tom-1.mp3");
-audio.play()
 }
 
 
@@ -33,9 +31,59 @@ for(var i=0; i< document.querySelectorAll(".drum").length; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-            let key = this;
+            let keyelem = this.innerHTML;
 
-            key.style.color = "white"
+
+            
+switch (keyelem) {
+    case "w": 
+let tom1 = new Audio("./sounds/tom-1.mp3");
+tom1.play()
+        
+        break;
+
+case "a":
+    
+let tom2 = new Audio("./sounds/tom-2.mp3");
+tom2.play() 
+break;
+
+
+case "s":
+    
+let tom3 = new Audio("./sounds/tom-3.mp3");
+tom3.play() 
+break;
+
+
+case "d":
+    
+let tom4 = new Audio("./sounds/tom-4.mp3");
+tom4.play() 
+break;
+
+
+case "j":
+    
+let snare = new Audio("./sounds/snare.mp3");
+snare.play() 
+break;
+
+
+case "k":
+    
+let crash = new Audio("./sounds/crash.mp3");
+crash.play() 
+break;
+
+
+case "l":
+    
+let kick = new Audio("./sounds/kick-bass.mp3");
+kick.play() 
+break;
+}
+            
         
     })
 }
